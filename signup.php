e0229db439a22ab08df8f13a3b2aea8ab47289db
Rajeev Singla <63499957+rajeevsingla780@users.noreply.github.com>
2021-05-19 12:54:54 +0530

    Update signup.php

--- signup.php
+++ signup.php
@@ -8,8 +8,8 @@ $cleardb_db = substr($cleardb_url["path"],1);
 $active_group = 'default';
 $query_builder = TRUE;
 // Connect to DB
-$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
-?>
+$con = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+
 
   try{
     
